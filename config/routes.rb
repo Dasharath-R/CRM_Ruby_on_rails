438ec6ee4c899a4d28bf4248b1db06f85e8d271f
Dasharath R <dasharathzoro@gmail.com>
2021-10-01 16:34:00 +0530

    changed route path to leads after login

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   resources :leads
   #get 'home/index'
   get 'home/about'
-  root 'home#index'
-  #root 'leads#index'
+  #root 'home#index'
+  root 'leads#index'
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
